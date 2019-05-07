40636803e9bfa158fdea964c8ae5f97d461b3927
unknown <brian.jang11@gmail.com>
2019-05-07 14:23:47 +0900

    add github access key

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -3,9 +3,12 @@ const GitHub = require('../lib/github')
 describe('Integration with GitHub API', () => {  
   let github
 
+  // ACCESS_TOKEN=4dd789a899240fb46dceac3788719b252fd253f9
+
   beforeAll(() => {
     github = new GitHub({
-      accessToken: process.env.ACCESS_TOKEN,
+      // accessToken: process.env.ACCESS_TOKEN,
+      accessToken: '4dd789a899240fb46dceac3788719b252fd253f9',
       baseURL: 'https://api.github.com',
     })
   })
